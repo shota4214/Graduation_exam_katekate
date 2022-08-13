553732ece80f362d1b3ff25cd3898341a5c3de2b
shota4214 <seshiruff4214@yahoo.co.jp>
2022-08-13 13:42:35 +0900

    :sparkles: ユーザshowに総like,favorite,PV数をそれぞれ表示する機能を実装 #12

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,5 +10,14 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @user_age = @user.age
     @articles = @user.articles.all.order("created_at DESC")
+    user_articles = @user.articles
+    @like_count_all = 0
+    @favorite_count_all = 0
+    @impression_count_all = 0
+    user_articles.each do |article|
+      @like_count_all += article.likes.count
+      @favorite_count_all += article.favorites.count
+      @impression_count_all += article.impressions_count
+    end
   end
 end
