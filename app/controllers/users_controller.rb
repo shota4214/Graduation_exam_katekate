79a8383066ea7dc4811c803433ed69d5b8bb6426
shota4214 <seshiruff4214@yahoo.co.jp>
2022-08-16 15:43:42 +0900

    :sparkles: ユーザ情報更新時ウィザード形式の入力フォーム実装完了# 13

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,18 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, only: %i[index follow_index update edit_step1 edit_step2 edit_step3 edit_step4]
+  before_action :user_find, only: %i[show follow_index edit_step1 edit_step2 edit_step3 edit_step4 update]
 
   def index
     @users = current_user.followings && current_user.followers if user_signed_in?
   end
 
   def follow_index
-    @user = User.find(params[:id])
     @following_users = @user.followings
     @follower_users = @user.followers
     @users = current_user.followings && current_user.followers if user_signed_in?
   end
 
   def show
-    @user = User.find(user_id)
     @user_age = @user.age
     @articles = @user.articles.all.order("created_at DESC").page(params[:page]).per(5)
     user_articles = @user.articles
@@ -28,45 +27,43 @@ class UsersController < ApplicationController
   end
 
   def edit_step1
-    @user = User.find(user_id)
   end
 
   def edit_step2
+    render :edit_step1 and return if params[:back]
+    session[:image] = params[:user][:image]
     session[:name] = user_params[:name]
     session[:email] = user_params[:email]
     session[:birthday] = Date.new(params[:user]["birthday(1i)"].to_i, params[:user]["birthday(2i)"].to_i, params[:user]["birthday(3i)"].to_i)
     session[:sex] = user_params[:sex]
     session[:prefectures] = user_params[:prefectures]
-    @user = User.find(user_id)
+    @user.image = params[:user][:image]
   end
 
   def edit_step3
-    session[:image] = params[:user][:image]
+    render :edit_step1 and return if params[:back]
     session[:image_cache] = params[:user][:image_cache]
-    # session[:image] = user_params[:image].read
-    # session[:image_cache] = user_params[:image_cache].read
     session[:introduction] = user_params[:introduction]
-    @user = User.find(user_id)
   end
 
   def edit_step4
+    render :edit_step2 and return if params[:back]
     session[:twitter_url] = user_params[:twitter_url]
     session[:instagram_url] = user_params[:instagram_url]
     session[:tiktok_url] = user_params[:tiktok_url]
     session[:facebook_url] = user_params[:facebook_url]
     session[:youtube_url] = user_params[:youtube_url]
     session[:website_url] = user_params[:website_url]
-    @user = User.find(user_id)
   end
 
   def update
-    @user = User.find(user_id)
+    render :edit_step3 and return if params[:back]
     session[:password] = user_params[:password]
     session[:password_confirmation] = user_params[:password_confirmation]
-    session[:id] = params[:id]
-    if @user.update!(image: session[:image], image_cache: session[:image_cache], name: session[:name], email: session[:email], birthday: session[:birthday], sex: session[:sex], prefectures: session[:prefectures], introduction: session[:introduction], password: session[:password], password_confirmation: session[:password_confirmation], twitter_url: session[:twitter_url], instagram_url: session[:instagram_url], tiktok_url: session[:tiktok_url], facebook_url: session[:facebook_url], youtube_url: session[:youtube_url], website_url: session[:website_url], id: session[:id])
+    @user.image.retrieve_from_cache! session[:image_cache]
+    if @user.update!(image: session[:image_cache], name: session[:name], email: session[:email], birthday: session[:birthday], sex: session[:sex], prefectures: session[:prefectures], introduction: session[:introduction], password: session[:password], password_confirmation: session[:password_confirmation], twitter_url: session[:twitter_url], instagram_url: session[:instagram_url], tiktok_url: session[:tiktok_url], facebook_url: session[:facebook_url], youtube_url: session[:youtube_url], website_url: session[:website_url])
       sign_in(@user, bypass: true) if current_user.id == @user.id
-      redirect_to articles_path, notice: '編集しました'
+      redirect_to articles_path, notice: 'ユーザー情報を更新しました'
     else
       redirect_to user_path, notice: '更新失敗しました'
     end
@@ -76,5 +73,9 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :prefectures, :birthday, :sex, :image, :image_cache, :introduction, :password, :password_confirmation, :current_password, :twitter_url, :instagram_url, :tiktok_url, :facebook_url, :youtube_url, :website_url)
+  end
+
+  def user_find
+    @user = User.find(params[:id])
   end
 end
