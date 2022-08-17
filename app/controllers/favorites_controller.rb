ac74623f575da6b222b60bee6868907a60ac960e
shota4214 <seshiruff4214@yahoo.co.jp>
2022-08-18 00:08:53 +0900

    :pencil2: favorite/controller不要なコードを削除 #27

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,15 +4,11 @@ class FavoritesController < ApplicationController
   def create
     @article = Article.find(params[:article_id])
     Favorite.create(user_id: current_user.id, article_id: params[:article_id])
-    # favorite = current_user.favorites.create(article_id: params[:article_id])
-    # redirect_to articles_path, notice: "#{favorite.article.user.name}さんの記事をお気に入りしました"
   end
 
   def destroy
     @article = Article.find(params[:id])
     Favorite.find_by(user_id: current_user.id, article_id: params[:id]).destroy
-    # favorite = current_user.favorites.find_by(id: params[:id]).destroy
-    # redirect_to articles_path, notice: "#{favorite.article.user.name}さんの記事をお気に入り解除しました"
   end
 
   def index
