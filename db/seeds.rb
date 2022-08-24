6d14048447aa9c4657371dd6f2e41a9eb2d1db44
shota4214 <seshiruff4214@yahoo.co.jp>
2022-08-24 13:44:39 +0900

    :art: ユーザーページデザインの実装と記事デザインの修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,13 @@
-User.create!(name: "admin", email: "admin@admin.com", password: "12345678", admin: "true")
+User.create!(name: "テスト管理者", email: "admin@test.com", password: "12345678", admin: "true")
 10.times do |n|
-  User.create!(name: "test_user#{n + 1}", email: "test#{n + 1}@test.com", password: "12345678")
+  User.create!(name: "テストユーザー#{n + 1}", email: "test#{n + 1}@test.com", password: "12345678")
 end
 
 User.all.each do |user|
-  user.articles.create!(type: 0, title: "article_title", content: "article_content(#{user.name})")
-  user.articles.create!(type: 1, title: "question_title", content: "question_content(#{user.name})")
+  user.articles.create!(type: 0, title: "記事タイトル", content: "article_content(#{user.name})")
+  user.articles.create!(type: 1, title: "質問タイトル", content: "question_content(#{user.name})")
+end
+
+10.times do |n|
+  Favorite.create!(following_id: 1, follower_id: (n + 1))
 end
