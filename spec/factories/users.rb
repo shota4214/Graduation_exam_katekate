3d738496f236bf56bd075cfe7081be9df4872d89
shota4214 <seshiruff4214@yahoo.co.jp>
2022-08-25 00:14:36 +0900

    :white_check_mark: RspecでのSystem specテストとModel specテスト完了 #32

---
+++ spec/factories/users.rb
@@ -0,0 +1,27 @@
+FactoryBot.define do
+  factory :user do
+    name { "test_name" }
+    email { "test@test.com" }
+    password { "12345678" }
+    password_confirmation { "12345678" }
+  end
+  factory :second_user, class: User do
+    name { "second_name" }
+    email { "second@test.com" }
+    password { "12345678" }
+    password_confirmation { "12345678" }
+  end
+  factory :third_user, class: User do
+    name { "third_name" }
+    email { "third@test.com" }
+    password { "12345678" }
+    password_confirmation { "12345678" }
+  end
+  factory :admin_user, class: User do
+    name { "admin_name" }
+    email { "admin@test.com" }
+    admin { "true" }
+    password { "12345678" }
+    password_confirmation { "12345678" }
+  end
+end
