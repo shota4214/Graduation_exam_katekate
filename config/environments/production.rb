36891e6ab7a627585fc0eebd6fad32d15dfdede6
shota4214 <seshiruff4214@yahoo.co.jp>
2022-08-26 12:49:07 +0900

    AWSメール送信エラー対応

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -114,7 +114,7 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
-    domain: 'katekate.com',
+    domain: 'katekate.blog',
     address: 'smtp.sendgrid.net',
     port: 587,
     authentication: :plain,
