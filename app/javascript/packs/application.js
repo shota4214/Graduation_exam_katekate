6c63d57d7e332ffa847136dc3ef3220ee09e9c72
shota4214 <seshiruff4214@yahoo.co.jp>
2022-09-04 18:59:03 +0900

    :art: ヘッダーにバーガーメニュー採用

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,3 +16,25 @@ import '@fortawesome/fontawesome-free/js/all'
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+document.addEventListener('DOMContentLoaded', () => {
+  // ナビゲーションバーガー（navbar-burgerクラスを持つすべての要素）を取得します。
+  const $navbarBurgers = document.querySelectorAll('.navbar-burger');
+  // ナビゲーションバーガーがあるかどうかを確認します。
+  if ($navbarBurgers.length > 0) {
+    // すべてのナビゲーションバーガーをループします。
+    $navbarBurgers.forEach( el => {
+      // ナビゲーションバーガーにクリックイベントを追加します。
+      el.addEventListener('click', () => {
+        // ナビゲーションバーガーのdata-target属性の値を取得します。
+        const target = el.dataset.target;
+        // メニュー（data-target属性の値をIDとして持つ要素）を取得します。
+        const $target = document.getElementById(target);
+        // ナビゲーションバーガーでis-activeクラスを切り替えます。
+        el.classList.toggle('is-active');
+        // メニューでis-activeクラスを切り替えます。
+        $target.classList.toggle('is-active');
+      });
+    });
+  }
+});
