bfcc70ed79681c5d62715dde528f9540308f3a36
shota4214 <seshiruff4214@yahoo.co.jp>
2022-09-16 00:05:12 +0900

    :sparkles: 通知機能メソッド作成

--- app/models/article.rb
+++ app/models/article.rb
@@ -12,4 +12,19 @@ class Article < ApplicationRecord
   has_many :notifications, dependent: :destroy
   is_impressionable counter_cache: true
   enum type: {article: 0, question: 1}
+
+  def create_notification_like!(current_user)
+    temp = Notification.where(["visitor_id = ? and visited_id = ? and article_id = ? and action = ?", current_user.id, user_id, id, "like"])
+    if temp.blank?
+      notification = current_user.active_notifications.new(
+        article_id: id,
+        visited_id: user_id,
+        action: "like"
+      )
+      if notification.visitor_id == notification.visited_id
+        notification.checked = true
+      end
+      notification.save if notification.valid?
+    end
+  end
 end
